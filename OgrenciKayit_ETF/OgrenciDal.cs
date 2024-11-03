293f9954bce5e23f8735ac6b486aa9754de73063
evinyanitoz <eyanitoz@gmail.com>
2024-11-03 19:01:47 +0300

    EDITED OGRENCIDAL

--- OgrenciKayit_ETF/OgrenciDal.cs
+++ OgrenciKayit_ETF/OgrenciDal.cs
@@ -9,70 +9,30 @@ namespace OgrenciKayit_ETF
 {
     public class OgrenciDal
     {
+      OgrenciKayitContext ogrenciKayitContext = new OgrenciKayitContext();
+        
         public List<Ogrenci> OgrenciListele() {
+       return   ogrenciKayitContext.Ogrenci.ToList();
 
-            using (OgrenciKayitContext ogrenciKayitContext=new OgrenciKayitContext()) {
-
-
-
-              return   ogrenciKayitContext.Ogrenci.ToList();
-            
-            
-            }
-        
-        
-        
         }
         public void OgrenciEkle(Ogrenci ogrenci) {
 
-
-            using (OgrenciKayitContext ogrenciKayitContext = new OgrenciKayitContext())
-            {
-
-
-
-              ogrenciKayitContext.Ogrenci.Add(ogrenci);
+                 ogrenciKayitContext.Ogrenci.Add(ogrenci);
                 ogrenciKayitContext.SaveChanges();
-
-
-            }
-
 
         }
         public void OgrenciSil(Ogrenci ogrenci)
         {
 
-
-            using (OgrenciKayitContext ogrenciKayitContext = new OgrenciKayitContext())
-            {
-
-
-
                 var entity = ogrenciKayitContext.Entry(ogrenci);
                 entity.State = EntityState.Deleted;
                 ogrenciKayitContext.SaveChanges();
 
-
-            }
-
-
         }
         public void OgrenciGuncelle(Ogrenci ogrenci)
         {
-
-
-            using (OgrenciKayitContext ogrenciKayitContext = new OgrenciKayitContext())
-            {
-
-
-
                 var entity = ogrenciKayitContext.Entry(ogrenci);
                 entity.State = EntityState.Modified;
-                ogrenciKayitContext.SaveChanges();
-
-            }
-
-
-        }
+                ogrenciKayitContext.SaveChanges();  }
     }
 }
